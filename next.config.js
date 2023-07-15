aa33c09f3a68ed05044e190fbbb5a4f85bcaaa0d
agasefamircan <agasefemircanov@gmail.com>
2023-07-16 01:18:37 +0400

    added logo

--- next.config.js
+++ next.config.js
@@ -7,12 +7,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true'
-// })
-
-// module.exports = withBundleAnalyzer({
-//   env: {
-//       NEXT_PUBLIC_ENV: 'PRODUCTION', 
-//   },
-// })```
